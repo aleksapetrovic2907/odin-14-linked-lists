520f83e8ad10ada0100419df31f6898dda49abbb
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-25 16:29:11 +0100

    implement getters for head & tail in LinkedList class

--- js/LinkedList.js
+++ js/LinkedList.js
@@ -2,4 +2,18 @@ export default class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  get head() {
+    return this.head;
+  }
+
+  get tail() {
+    let current = this.head;
+
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+
+    return current;
+  }
 }
